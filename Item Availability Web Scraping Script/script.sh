8e3c92ecb7750d633d1e93944477252384aa9c62
SebastianHall69 <sebastianhall13@gmail.com>
2021-01-15 12:36:03 -0800

    Deleted object files and uncommented section in item checked

--- Item Availability Web Scraping Script/script.sh
+++ Item Availability Web Scraping Script/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # variables
-#amz_link="https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_1"
+amz_link="https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_1"
 bb_link="https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
 wal_link="https://www.walmart.com/ip/PlayStation-5-Console/363472942"
 
-#amz_file="amazon.html"
+amz_file="amazon.html"
 bb_file="bestbuy.html"
 wal_file="walmart.html"
 
@@ -20,33 +20,33 @@ while true; do
 
 # Download files
 printf "downloading pages...\n\n"
-#wget -q -T 30 -O $amz_file -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36" $amz_link
-wget -q -T 30 -O $bb_file $bb_link
+wget -q -T 30 -O $amz_file -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36" $amz_link
+wget -q -T 30 -O $bb_file $bb_link # For some reason it takes about 10 seconds to download from best buy?
 wget -q -T 30 -O $wal_file -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36" $wal_link
 
 
 # Parse files
-#amz=$(grep "Currently unavailable" $amz_file)
+amz=$(grep "Currently unavailable" $amz_file)
 bb=$(grep "btn-disabled" $bb_file)
 wal=$(grep -o "<b>Out of stock</b>" $wal_file)
 
 # Check status of amazon
-#if [[ -z $amz ]]
-#then
-#       	echo "PS5 at amazon"
-#	if [[ $amz_cnt -eq 0 ]]
-#	then
-#      		firefox $amz_link &
-#		((amz_cnt = amz_cnt + 1))
-#	fi
-#else
-#       echo "No Amazon"
-#fi
+if [[ -z $amz ]]
+then
+       	echo "Item at amazon"
+	if [[ $amz_cnt -eq 0 ]]
+	then
+      		firefox $amz_link &
+		((amz_cnt = amz_cnt + 1))
+	fi
+else
+       echo "No Amazon"
+fi
 
 # Check status of best buy
 if [[ -z $bb ]]
 then
-       	echo "PS5 at best buy"
+       	echo "Item at best buy"
 	if [[ $bb_cnt -eq 0 ]]
 	then
        		firefox $bb_link &
@@ -59,7 +59,7 @@ fi
 # Check status of walmart
 if [[ -z $wal ]]
 then
-       	echo "PS5 at walmart"
+       	echo "Item at walmart"
 	if [[ $wal_cnt -eq 0 ]]
 	then
        		firefox $wal_link &
